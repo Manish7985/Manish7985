ee26607e7e34872da2ba2da843d6cbc77ff5e47e
Manish7985 <94455351+Manish7985@users.noreply.github.com>
2021-11-16 23:02:45 +0545

    Create demo.c

---
+++ demo.c
@@ -0,0 +1,15 @@
+//Basic program to print a message
+
+#include<stdio.h>   /* header file: standard input-output
+                     have 2 funtions:  printf(), scanf()  
+					  # : preprocessor directive  */
+					  
+int main()         // main function with return type integer: 
+                   //from here the program execution starts
+{
+	printf("This is my first class of cse101 for 2021...");
+	          //prints the text written within ""
+	          
+	return 0;   /* 0 return value means the prgm executes successfuly
+	               1 return value means the prgm have errors */ 
+}
