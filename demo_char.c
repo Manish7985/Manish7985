cc0f9222b16284e642acf9fb7b5c02b4d0d96ceb
Manish7985 <94455351+Manish7985@users.noreply.github.com>
2021-11-16 23:06:48 +0545

    Create demo_char.c

---
+++ demo_char.c
@@ -0,0 +1,13 @@
+/Demo of character datatype
+#include<stdio.h>
+void main()
+{
+	char gender1 = 'F';
+	char gender2 = 'M';
+	
+	printf("First Gender is %c", gender1);
+	printf("\tSecond Gender is %c",gender2);
+	 // \n: new line operator, to print in the next line
+	//  \t: tab operator, include 4 spaces
+}
+
