a3f83c1688fc1285e112fa99b868a52a29480256
Manish7985 <94455351+Manish7985@users.noreply.github.com>
2021-11-16 23:00:06 +0545

    Create Add input.c

---
+++ Add input.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+
+	int x, y;
+	printf("Enter the first number:");
+	scanf("%d",&x);
+
+	printf("Enter the second number:");
+	scanf("%d",&y);
+
+	int sum = x+y;
+	printf("Your sum = %d",sum);
+
+	return 0;
+
+}
