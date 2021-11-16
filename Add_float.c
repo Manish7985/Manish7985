31a138ede22dfd2debf44a17a031f23f6363217c
Manish7985 <94455351+Manish7985@users.noreply.github.com>
2021-11-16 23:04:40 +0545

    Create Add_float.c

---
+++ Add_float.c
@@ -0,0 +1,16 @@
+//Adding 3 float numbers
+#include<stdio.h>
+int main()
+{
+	//float a, b, c;  //float takes upto 6 decimal places
+	double a,b,c;
+	a = 100.23;
+	b = 89.5;
+	c = 12.129;
+	
+	//float sum = a+b+c;
+	//printf("Sum of 3 float values = %f",sum);
+	double sum = a+b+c;
+	printf("Sum of 3 double values = %lf", sum);
+	
+}
